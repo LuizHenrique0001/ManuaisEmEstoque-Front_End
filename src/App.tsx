745a944a86b0d62abf91e2abe483bb2ccb4cef6e
Luizhenrique0001 <rodriguesluizhenrique316@gmail.com>
2025-10-10 10:04:23 -0300

    Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import List from "./Components/List.tsx";
+// import List from "./Components/List.tsx";
 import {useCarData} from "./Hooks/UseCarData.ts";
+import Menu from "./Components/Menu.tsx";
+import List from "./Components/List.tsx";
 
 function App() {
 
     const {data} = useCarData();
 
-  return (
-    <div className=" w-screen h-screen flex justify-center p-6 bg-slate-400">
-        <div className="bg-slate-300 w-150 rounded-xl">
-            <ul className="p-6 text-slate-700">
-                <li className="flex">
-                    <h1 className="flex-1"><b>Chassi</b></h1>
-                    <h1 className="flex-1"><b>Modelo</b></h1>
-                    <h1 className="flex-1"><b>Recebimento</b></h1>
-                </li>
-                {data?.map((item,key) => (<List key={key} chassi={item.chassi} model={item.model} dataReceived={item.dataReceived} />))}
-
-            </ul>
+    return (
+    <div className=" w-screen h-screen justify-center p-2 bg-slate-400">
+        <Menu/>
+        <div className='flex justify-center p-6'>
+            <div className="bg-slate-300 w-150 rounded-xl">
+                <ul className="p-6 text-slate-700">
+                    <li className="flex">
+                        <h1 className="flex-1"><b>Chassi</b></h1>
+                        <h1 className="flex-1"><b>Modelo</b></h1>
+                        <h1 className="flex-1"><b>Recebimento</b></h1>
+                    </li>
+                    {data?.map((item,key) => (<List key={key} chassi={item.chassi} model={item.model} dataReceived={item.dataReceived} />))}
+                </ul>
+            </div>
         </div>
     </div>
 
---
+++ src/Components/Menu.tsx
@@ -0,0 +1,8 @@
+function Menu(){
+    return (
+        <div className='bg-slate-200 w-full rounded-md text-2xl text-center font-bold font-serif text-slate-700'>
+            <h1 className='p-2'>Manuais em Estoque</h1>
+        </div>
+    );
+}
+export default Menu;
